f0c2f1a374f3669e74fa42ef99ee23ccf7cf5014
Lawrence <lawrencekkyalo@gmail.com>
2019-04-03 09:23:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var Twit = require('twit');
 var T = new Twit({
-	consumer_key: '';
-	consumer_secret: '';
-	access_token: '';
-	access_token_secret: '';
+	consumer_key: '',
+	consumer_secret: '',
+	access_token: '',
+	access_token_secret: ''
 });
 
 T.get('account/verify_credentials', {
